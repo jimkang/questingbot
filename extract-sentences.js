7b0acdf82299c6026bcff0f83ac7201a4088272b
Jim Kang <jimkang@gmail.com>
2016-04-28 09:05:47 -0400

    Get sentence extraction going.

---
+++ extract-sentences.js
@@ -0,0 +1,42 @@
+var ParseEnglish = require('parse-english');
+var english = new ParseEnglish();
+var pluck = require('lodash.pluck');
+var flatten = require('lodash.flatten');
+
+function ExtractSentences(createOpts) {
+  return extractSentences;
+  
+  function extractSentences(text) {
+    var cleanedText = text.replace(/https*:\/\/.*\b/g, '');
+    var root = english.parse(cleanedText);
+    return root.children.reduce(collectSentencesFromParagraph, []);
+  }
+
+  // We don't care about paragraph groupings for now. We just want to get the
+  // (cleaned) sentences out of them.
+  function collectSentencesFromParagraph(sentences, paragraphNode) {
+    if (paragraphNode.children) {
+      sentences = sentences.concat(
+        paragraphNode.children.map(wordArrayFromSentenceNode)
+      );
+    }
+    return sentences;
+  }
+
+  function wordArrayFromSentenceNode(sentenceNode) {
+    var wordNodes = sentenceNode.children.filter(isWord);
+    // If dependency-parser handles punctuation someday, we need to leave that in.
+    var textNodes = flatten(pluck(wordNodes, 'children')).filter(isText);
+    return pluck(textNodes, 'value');
+  }
+}
+
+function isWord(n) {
+  return n.type === 'WordNode';
+}
+
+function isText(n) {
+  return n.type === 'TextNode';
+}
+
+module.exports = ExtractSentences;
---
+++ tests/extract-sentences-tests.js
@@ -0,0 +1,75 @@
+var test = require('tape');
+var ExtractSentences = require('../extract-sentences');
+
+var extractSentences = ExtractSentences({
+});
+
+var testCases = [
+    {
+      text: 'Searching Airbnb for a spot in Nice, France when magic happened... https://t.co/DKGxvj3vyE',
+      expected: [ [ 'Searching',
+        'Airbnb',
+        'for',
+        'a',
+        'spot',
+        'in',
+        'Nice',
+        'France',
+        'when',
+        'magic',
+        'happened' ] ]
+    [ [ 'Searching',
+        'Airbnb',
+        'for',
+        'a',
+        'spot',
+        'in',
+        'Nice',
+        'France',
+        'when',
+        'magic',
+        'happened' ] ]
+    },
+    {
+      text: 'Today\'s niche observation: the claymore is indespensable to deal with the toxic rats so you can properly get under The Royal Rat Authority',
+      expected: [ [ 'Today',
+        's',
+        'niche',
+        'observation',
+        'the',
+        'claymore',
+        'is',
+        'indespensable',
+        'to',
+        'deal',
+        'with',
+        'the',
+        'toxic',
+        'rats',
+        'so',
+        'you',
+        'can',
+        'properly',
+        'get',
+        'under',
+        'The',
+        'Royal',
+        'Rat',
+        'Authority' ] ]
+    },
+    {
+      text: 'Fire Hand-light',
+      expected: [ [ 'Fire', 'Hand', 'light' ] ]
+    }
+];
+
+testCases.forEach(runTest);
+
+function runTest(testCase) {
+  test('Extract sentence test: ' + testCase.text.substr(0, 50), extractTest);
+
+  function extractTest(t) {
+    t.deepEqual(extractSentences(testCase.text), testCase.expected, 'Sentences are correct.');
+    t.end();
+  }
+}
